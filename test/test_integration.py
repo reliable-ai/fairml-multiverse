cfa7aa94f31cf524858bfc3d919eabf3973e0145
Jan Simson <git@simson.io>
2024-03-21 15:04:52 +0100

    Add small test case to verify whether code / installation is working

---
+++ test/test_integration.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+import shutil
+import unittest
+import pandas as pd
+
+import os
+
+ROOT_DIR = Path(__file__).parent.parent
+TEMP_DIR = ROOT_DIR / "test" / "temp"
+
+
+def count_files(dir, glob):
+    return len(list(dir.glob(glob)))
+
+
+class TestMultiverseAnalysisIntegration(unittest.TestCase):
+    """
+    Test the multiverse analysis script end-to-end, running a small scale
+    test multiverse analysis and comparing the final data to a stored snapshot.
+    """
+
+    def test_end_to_end(self):
+        # Create and clear output directory
+        output_dir = TEMP_DIR
+        shutil.rmtree(output_dir, ignore_errors=True)
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        # Run a test multiverse analysis
+        os.system(
+            f"python multiverse_analysis.py --mode test --output-dir {output_dir}"
+        )
+
+        # Check whether all expected files are there
+        assert count_files(output_dir, "runs/1/data/*.csv.gz") == 1
+        assert count_files(output_dir, "runs/1/data/*.csv") == 2
+        assert count_files(output_dir, "runs/1/notebooks/*.ipynb") == 2
+        assert count_files(output_dir, "counter.txt") == 1
+        assert count_files(output_dir, "multiverse_grid.json") == 1
+
+        # Compare final data to stored snapshot
+        # Please note: any change to the analysis scripts will break this test
+        # Either copy over the latest aggregated data file or disable this test altogether
+        filename_df_agg = "agg_1_run_outputs.csv.gz"
+        columns_to_ignore = ["execution_time"]
+        df_snap = pd.read_csv(ROOT_DIR / "test" / "snapshots" / filename_df_agg).drop(
+            columns=columns_to_ignore
+        )
+        df_new = pd.read_csv(output_dir / "runs/1/data/" / filename_df_agg).drop(
+            columns=columns_to_ignore
+        )
+        pd.testing.assert_frame_equal(df_snap, df_new)
+
+
+if __name__ == "__main__":
+    unittest.main()
